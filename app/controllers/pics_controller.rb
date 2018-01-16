daf72342dba86fa4b4ca26085d78288e942d9c7f
Armando Ruiz <armando.ruiz@industrie.co>
2018-01-16 15:51:52 +1100

    Created form partial and the create controller

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -4,11 +4,30 @@ class PicsController < ApplicationController
   end
 
   def new
+    @pic ||= Pic.new
+  end
+
+  def create
+    @pic ||= Pic.new(pic_params)
+
+    if @pic.save
+      flash[:message] = "Image created"
+      redirect_to root_path
+    else
+      flash[:message] = "Image NOT created"
+      render new
+    end
   end
 
   def edit
   end
 
   def show
+  end
+
+  private
+
+  def pic_params
+    params.require(:pic).permit(:title, :description, :image)
   end
 end
