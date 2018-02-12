d8edaa24a9df24f219ef4bfedbb27e8b3dd285d4
Armando Ruiz <armando.ruiz@industrie.co>
2018-02-12 14:11:18 +1100

    Implemented likes in the application using gem act as votable

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # changed the route names, removed the users from: localhost: 3000/users/sign_up amd chenged sign_in for login, sign_out for logout and
   # sign_up for register
   root to: 'pics#index'
-  resources :pics
+  resources :pics do
+    member do
+      put 'like' => 'pics#vote'
+    end
+  end 
   get 'user_pics' => 'pics#user_pics', as: 'user_pics_index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
